633c1e286084fbb48280b0e549f9b1d502142a72
Gulsara1809 <92300038+Gulsara1809@users.noreply.github.com>
2021-10-18 12:25:02 +0600

    Add files via upload

--- MainClassTest03.java
+++ MainClassTest03.java
@@ -1,23 +1,26 @@
 import org.junit.Test;
 
 public class MainClassTest03 {
-    String class_string =  "Hello";
+
+    String class_string =  "test content which contains substring like helloworld";
+    String my_string = "Hello";
+    String my_string2 = "hello";
+
     @Test
     public void testGetClassString()
     {
         this.getClassString();
-        if ( class_string == "Hello" | class_string == "hello" )
-        {
-        System.out.println(class_string);
 
-    }
-
-        else { System.out.println("Тест падает");
+        if ( class_string.contains(my_string) || class_string.contains(my_string2) ) {
+            System.out.print(true);
+        } else {
+            throw new java.lang.Error("text do not contain the substring");
         }
     }
+
+
     public String getClassString()
     {
-
         return class_string;
     }
 
