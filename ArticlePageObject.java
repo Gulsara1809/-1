9fae9e8abc0288a38ce3c6e88ac0f84afa4f0b1a
Gulsara1809 <92300038+Gulsara1809@users.noreply.github.com>
2021-12-12 22:34:08 +0600

    Add files via upload

--- ArticlePageObject.java
+++ ArticlePageObject.java
@@ -1,14 +1,19 @@
 package lib.ui;
 
 import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class ArticlePageObject extends MainPageObject
 {
     private static final String
-            TITLE ="org.wikipedia:id/view_page_title_text",
-    FOOTER_ElEMENT = "//*[@text = 'View page in browser']";
+            TITLE ="id:org.wikipedia:id/view_page_title_text",
+    FOOTER_ElEMENT = "xpath://*[@text ='View page in browser']",
+   OPTIONS_BUTTON = "xpath://android.widget.ImageView[@content-desc='More option']",
+    OPTIONS_ADD_TO_MY_LIST_BUTTON = "xpath://*[@text='Add to reading list']",
+    ADD_TO_MY_LIST_OVERLAY = "id:org.wikipedia:id/anboarding_button",
+MY_LIST_NAME_INPUT = "id:org.wikipedia:id/text_input",
+            MY_LIST_OK_BUTTON ="xpath://[@text='OK']",
+    CLOSE_ARTICLE_BUTTON = "xpath://android.widget.ImageButton[@content-desc='Navigate up']";
 
     public ArticlePageObject (AppiumDriver driver)
     {
@@ -16,20 +21,67 @@ public class ArticlePageObject extends MainPageObject
     }
     public WebElement waitForTitleElement ()
     {
-        return this.waitForElementPresent(By.id(TITLE), "Cannot 11", 15);
+        return this.waitForElementPresent(TITLE, "Cannot 11", 15);
     }
     public String getArticleTitle ()
     {
-        WebElement title_element =waitForTitleElement();
+        WebElement title_element = waitForTitleElement();
         return title_element.getAttribute("text");
     }
     public void swipeToFooter ()
     {
         this.swipeUpToFindElement(
-                By.xpath(FOOTER_ElEMENT),
+                FOOTER_ElEMENT,
                  "Cannot find the end of article",
                 20
         );
 
     }
-}
+    public void  addArticleToMyList (String name_of_folder) {
+        this.waitForElementAndClick(
+              OPTIONS_BUTTON,
+                "Cannot find  button to open article option ",
+                5
+        );
+        this.waitForElementAndClick(
+                OPTIONS_ADD_TO_MY_LIST_BUTTON,
+                "Cannot find option to and article to reading list",
+                5
+        );
+        this.waitForElementAndClick(
+                ADD_TO_MY_LIST_OVERLAY,
+                "Cannot find 'Got it' tip overlay",
+                5
+        );
+        this.waitForElementAndClear(
+                MY_LIST_NAME_INPUT,
+                "Cannot find input to set name of article folder",
+                5
+        );
+        this.waitForElementAndSendKeys(
+             MY_LIST_NAME_INPUT,
+                name_of_folder,
+                "Cannot find text into articles folder input",
+                5
+        );
+        this.waitForElementAndClick(
+                MY_LIST_OK_BUTTON,
+                "Cannot press OK button",
+                5
+
+        );
+    }
+        public void closeArticle()
+    {
+        this.waitForElementAndClick(
+               CLOSE_ARTICLE_BUTTON,
+                "Cannot close article. cannot find X link",
+                5
+        );
+
+
+    }
+
+
+    }
+
